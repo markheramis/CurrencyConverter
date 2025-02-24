817a8dbf87197d5b7435232c775b3e9c8a265d54
Mark Heramis <chumheramis@gmail.com>
2025-02-24 15:27:22 +0800

    add propper input for amount and currencies
    Signed-off-by: Mark Heramis <chumheramis@gmail.com>

--- src/main.rs
+++ src/main.rs
@@ -2,7 +2,7 @@ use dotenv::dotenv;
 use clap::Parser;
 
 use rust_decimal::Decimal;
-// use rust_decimal::prelude::*;
+use rust_decimal::prelude::*;
 
 #[derive(Parser, Debug)]
 #[command(version, about, long_about = None)]
@@ -27,26 +27,29 @@ fn main() {
     let exchange_rate_api_key = std::env::var("EXCHANGE_RATE_API_KEY").expect("EXCHANGE_RATE_API_KEY: must be set.");
 
     if args.amount.is_none() {
-        print!("Enter amount: ");
+        println!("Enter amount: ");
         let mut amount_input = String::new();
         std::io::stdin().read_line(&mut amount_input).unwrap();
-        args.amount = Some(Decimal::from(amount_input.trim()).expect("Invalid amount format!"));
+        args.amount = Some(Decimal::from_str(amount_input.trim()).expect("Invalid amount format!"));
     }
-    //match args.to {
-    //    Some(to) => {
-    //        println!("Currency to: {}", to);
-    //    }
-    //    None => {
-    //        println!("Currency to not set");
-    //    }
-    //}
-    //match args.from {
-    //    Some(from) => {
-    //        println!("Currency from: {}", from);
-    //    }
-    //    None => {
-    //        println!("Currency from not set");
-    //    }
-    //}
-    println!("Args: {:?}", &args);
+    if args.from.is_none() {
+        println!("Enter currency to convert from:");
+        let mut from_input = String::new();
+        std::io::stdin().read_line(&mut from_input).unwrap();
+        args.from = Some(from_input.trim().to_string()); // Fix applied here
+    }
+
+    if args.to.is_none() {
+        println!("Enter currency to convert into:");
+        let mut to_input = String::new();
+        std::io::stdin().read_line(&mut to_input).unwrap();
+        args.to = Some(to_input.trim().to_string()); // Added missing assignment
+    }
+    let url = format!(
+        "https://v6.exchangerate-api.com/v6/{}/pair/{}/{}",
+        exchange_rate_api_key,
+        args.from.unwrap(),
+        args.to.unwrap()
+    ); 
+    println!("URL: {}", url);
 }
