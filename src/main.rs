c8e0d0aa06c3318c2c92f026313d6d7a9b9d0123
Mark Heramis <chumheramis@gmail.com>
2025-02-24 17:23:48 +0800

    actually sending the request to the api endpoint and getting the response. also added the calculated result and a properly formatted output
    Signed-off-by: Mark Heramis <chumheramis@gmail.com>

--- src/main.rs
+++ src/main.rs
@@ -4,6 +4,10 @@ use clap::Parser;
 use rust_decimal::Decimal;
 use rust_decimal::prelude::*;
 
+use reqwest::Client;
+use reqwest::Response;
+use serde::Deserialize;
+
 #[derive(Parser, Debug)]
 #[command(version, about, long_about = None)]
 pub struct Args {
@@ -20,8 +24,18 @@ pub struct Args {
     pub to: Option<String>
 }
 
+#[derive(Deserialize, Debug)]
+struct ExchangeRateResponse {
+    result: String,
+    base_code: String,
+    target_code: String,
+    conversion_rate: f64,
+    time_last_update_utc: String,
+    time_next_update_utc: String,
+}
 
-fn main() {
+#[tokio::main]
+async fn main() {
     dotenv().ok();
     let mut args = Args::parse();
     let exchange_rate_api_key = std::env::var("EXCHANGE_RATE_API_KEY").expect("EXCHANGE_RATE_API_KEY: must be set.");
@@ -38,9 +52,8 @@ fn main() {
         std::io::Write::flush(&mut std::io::stdout()).expect("Failed to flush stdout");
         let mut from_input = String::new();
         std::io::stdin().read_line(&mut from_input).unwrap();
-        args.from = Some(from_input.trim().to_string()); // Fix applied here
+        args.from = Some(from_input.trim().to_uppercase());
     }
-
     if args.to.is_none() {
         print!("Enter currency to convert into: ");
         std::io::Write::flush(&mut std::io::stdout()).expect("Failed to flush stdout");
@@ -51,8 +64,20 @@ fn main() {
     let url = format!(
         "https://v6.exchangerate-api.com/v6/{}/pair/{}/{}",
         exchange_rate_api_key,
-        args.from.unwrap(),
-        args.to.unwrap()
+        args.from.clone().unwrap(),
+        args.to.clone().unwrap()
     ); 
-    println!("URL: {}", url);
+
+    let client: Client = reqwest::Client::new();
+
+    let response: Response = client.get(url).send().await.expect("Failed to send request");
+    let body: ExchangeRateResponse = response.json().await.expect("Failed to read response body");
+
+    let amount = args.amount.unwrap();
+    let conversion_rate = Decimal::from_f64(body.conversion_rate).unwrap();
+    let from = args.from.clone().unwrap();
+    let to = args.to.clone().unwrap();
+
+    let converted_amount = amount * conversion_rate;
+    println!("{} {} is {} {}", amount, from, converted_amount, to);
 }
